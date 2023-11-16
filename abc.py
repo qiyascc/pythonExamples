03ad7b46307aed466530f885b72bb027d577ccf7
Qiyas <107604130+qiyascc@users.noreply.github.com>
2023-11-16 12:15:11 +0400

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,14 +1,3 @@
 import random
 
-def dice_roll_simulation(roll_count):
-    double_six_count = 0
-    for _ in range(roll_count):
-        die1 = random.randint(1, 6)
-        die2 = random.randint(1, 6)
-        if die1 == 6 and die2 == 6:
-            double_six_count += 1
-    probability_of_double_six = double_six_count / roll_count
-    return probability_of_double_six
-
-probability = dice_roll_simulation(100)
-print(probability)
+print({f'{i}:{i}': sum(random.randint(1, 6) == i for _ in range(100)) / 100 for i in range(1, 7)})
